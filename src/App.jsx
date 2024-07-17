1b3abcd3c71d9e86463ba0a604d79ac7ec536aaa
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-17 10:12:15 -0300

    Add Card section on App (mapping all cards on data)

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Card from "./components/Card.jsx";
 import data from "./data.js";
 
 function App() {
+  const cards = data.map((card) => {
+    return <Card key={card.id} card={card} />;
+  });
   return (
     <>
       <Header />
+      <section className="cards-container">{cards}</section>
     </>
   );
 }
